aa6eae47e49bbe5298b24a3e9d66f051c7fb9dd7
zine hamdi <zinehamdi8@gmail.com>
2026-01-30 13:45:17 +0100

    fix: Minimal console.php and disable AuthServiceProvider temporarily

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,5 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\AuthServiceProvider::class,
+    // App\Providers\AuthServiceProvider::class,  // Temporarily disabled
 ];
