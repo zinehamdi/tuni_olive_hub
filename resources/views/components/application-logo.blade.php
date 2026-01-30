dcce8dcda9e4fdd67a8aa6f7499c693514bfee58
zine hamdi <zinehamdi8@gmail.com>
2026-01-30 13:28:36 +0100

    fix: Remove Console Kernel for Laravel 12, fix mobile logo, add Schedule to console.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/zintoop-logo.png') }}" alt="{{ __(app()->getLocale() === 'ar' ? 'brand.name_ar' : 'brand.name_latin') }} Logo" class="rounded-full object-cover" {{ $attributes }}>
+<img src="{{ asset('images/zintoop-logo.png') }}" alt="{{ __(app()->getLocale() === 'ar' ? 'brand.name_ar' : 'brand.name_latin') }} Logo" class="rounded-full object-cover max-w-full" {{ $attributes }}>
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Daily price ingestion job
+Schedule::job(new \App\Jobs\IngestDailyPrices)->dailyAt('06:00')->onOneServer()->withoutOverlapping();
